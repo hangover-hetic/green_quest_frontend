11e52310ebd95bd835eb52965f849c66b2101bf6
Piero <piero.neri@hotmail.fr>
2023-03-31 15:49:32 +0200

    Map view + get current location and center on + live update center

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,48 +1,180 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
 import 'package:go_router/go_router.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  LocationData? _currentLocation;
+  late final MapController _mapController;
+
+  bool _liveUpdate = false;
+  bool _permission = false;
+
+  String? _serviceError = '';
+
+  int interActiveFlags = InteractiveFlag.all;
+
+  //final Location _locationService = Location();
+
+  @override
+  void initState() {
+    super.initState();
+    _mapController = MapController();
+    initLocationService();
+  }
+
+  void initLocationService() async {
+
+    Location locationService = Location();
+
+    await locationService.changeSettings(
+      accuracy: LocationAccuracy.high,
+      interval: 1000,
+    );
+
+    LocationData? location;
+    PermissionStatus _permissionGranted;
+    bool serviceEnabled;
+    bool serviceRequestResult;
+
+
+
+    try {
+      serviceEnabled = await locationService.serviceEnabled();
+
+      if (serviceEnabled) {
+        final permission = await locationService.requestPermission();
+        _permission = permission == PermissionStatus.granted;
+
+        if (_permission) {
+          location = await locationService.getLocation();
+          _currentLocation = location;
+          locationService.onLocationChanged
+              .listen((LocationData result) async {
+            if (mounted) {
+              setState(() {
+                _currentLocation = result;
+
+                // If Live Update is enabled, move map center
+                if (_liveUpdate) {
+                  _mapController.move(
+                      LatLng(_currentLocation!.latitude!,
+                          _currentLocation!.longitude!),
+                      _mapController.zoom);
+                }
+              });
+            }
+          });
+        }
+      } else {
+        serviceRequestResult = await locationService.requestService();
+        if (serviceRequestResult) {
+          initLocationService();
+          return;
+        }
+      }
+    } on PlatformException catch (e) {
+      debugPrint(e.toString());
+      if (e.code == 'PERMISSION_DENIED') {
+        _serviceError = e.message;
+      } else if (e.code == 'SERVICE_STATUS_ERROR') {
+        _serviceError = e.message;
+      }
+      location = null;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Screen'),
+    LatLng currentLatLng;
+
+    // Until currentLocation is initially updated, Widget can locate to 0, 0
+    // by default or store previous location value to show.
+    if (_currentLocation != null) {
+      currentLatLng =
+          LatLng(_currentLocation!.latitude!, _currentLocation!.longitude!);
+    } else {
+      return const CircularProgressIndicator();
+    }
+
+    print("currentLatLng: $currentLatLng");
+
+    final markers = <Marker>[
+      Marker(
+        width: 80,
+        height: 80,
+        point: currentLatLng,
+        builder: (context) => const Icon(
+          Icons.location_on,
+          color: Colors.red,
+          size: 45,
+        ),
       ),
-      body: Padding(
+    ];
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: Padding( //body
         padding: const EdgeInsets.all(0),
-        child: Column(
+        child: Column( //div
           children: [
             Flexible(
               child: FlutterMap(
+                mapController: _mapController,
                 options: MapOptions(
-                  center: LatLng(51.5, -0.09),
-                  zoom: 5,
-                  enableScrollWheel: true,
-                  scrollWheelVelocity: 0.005,
+                  center: currentLatLng,
+                  zoom: 12,
+                  interactiveFlags: interActiveFlags,
                 ),
-                nonRotatedChildren: [
-                  AttributionWidget.defaultWidget(
-                    source: 'OpenStreetMap contributors',
-                    onSourceTapped: () {},
-                  ),
-                ],
                 children: [
                   TileLayer(
                     urlTemplate:
                     'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    errorImage: const NetworkImage('https://tile.openstreetmap.org/18/0/0.png'),
-
+                    userAgentPackageName: 'dev.fleaflet.flutter_map.example',
                   ),
+                  MarkerLayer(markers: markers),
                 ],
               ),
             ),
+
+
           ],
         ),
       ),
+      floatingActionButton: Builder(builder: (BuildContext context) {
+        return FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _liveUpdate = !_liveUpdate;
+
+              if (_liveUpdate) {
+                interActiveFlags = InteractiveFlag.rotate |
+                InteractiveFlag.pinchZoom |
+                InteractiveFlag.doubleTapZoom;
+
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text(
+                      'In live update mode only zoom and rotation are enable'),
+                ));
+              } else {
+                interActiveFlags = InteractiveFlag.all;
+              }
+            });
+          },
+          child: _liveUpdate
+              ? const Icon(Icons.location_on)
+              : const Icon(Icons.location_off),
+        );
+      }),
     );
   }
 }
