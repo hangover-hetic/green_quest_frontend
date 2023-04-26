27206a592f4162b460a6e6de44d6ddacf5c8c117
Timothée DURAND <timothee.durand70@gmail.com>
2023-04-26 21:18:11 +0200

    feat: link feed to event

--- lib/screens/event_details.dart
+++ lib/screens/event_details.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:go_router/go_router.dart';
 import 'package:green_quest_frontend/api/models/event.dart';
 import 'package:green_quest_frontend/api/service.dart';
-import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
 class EventdetailsScreen extends StatefulWidget {
@@ -29,16 +29,17 @@ class _EventdetailsScreenState extends State<EventdetailsScreen> {
       appBar: AppBar(
         title: const Text('My Screen'),
         leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              context.go('/');
-            }),
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            context.go('/');
+          },
+        ),
       ),
       body: FutureBuilder<Event>(
         future: event,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           }
           if (snapshot.hasError) {
             return Text('${snapshot.error}');
@@ -48,20 +49,20 @@ class _EventdetailsScreenState extends State<EventdetailsScreen> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(8),
                 child: Text(
                   event.title,
-                  style: TextStyle(
+                  style: const TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 20,
                   ),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(8),
                 child: Text(
                   event.description,
-                  style: TextStyle(
+                  style: const TextStyle(
                     fontSize: 16,
                   ),
                 ),
@@ -71,28 +72,33 @@ class _EventdetailsScreenState extends State<EventdetailsScreen> {
                 child: FlutterMap(
                   options: MapOptions(
                     center: LatLng(event.latitude, event.longitude),
-                    zoom: 13.0,
                   ),
                   children: [
                     TileLayer(
                       urlTemplate:
-                      'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                          'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                       userAgentPackageName: 'dev.fleaflet.flutter_map.example',
                     ),
                     MarkerLayer(
-                        markers: [
-                          Marker(
-                            width: 80.0,
-                            height: 80.0,
-                            point: LatLng(event.latitude, event.longitude),
-                            builder: (ctx) => Container(
-                              child: Icon(Icons.location_pin),
-                            ),
+                      markers: [
+                        Marker(
+                          width: 80,
+                          height: 80,
+                          point: LatLng(event.latitude, event.longitude),
+                          builder: (ctx) => Container(
+                            child: const Icon(Icons.location_pin),
                           ),
-                        ]),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
+              ElevatedButton(
+                  onPressed: () {
+                    context.go("/feed/${event.feedId}");
+                  },
+                  child: const Text("Go to feed")),
             ],
           );
         },
@@ -100,19 +106,19 @@ class _EventdetailsScreenState extends State<EventdetailsScreen> {
       floatingActionButton: SizedBox(
         width: 140,
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16),
           child: ElevatedButton(
             onPressed: () {},
-            child: Text(
-              'Sign Up',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-            ),
             style: ElevatedButton.styleFrom(
-              primary: Color(0xFF0E756E),
+              backgroundColor: const Color(0xFF0E756E),
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(30),
               ),
               padding: const EdgeInsets.symmetric(vertical: 16),
+            ),
+            child: const Text(
+              'Sign Up',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
             ),
           ),
         ),
